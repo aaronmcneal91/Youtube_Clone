4226a34f7510cb6668fb77a24e60fa5dc8bacdac
unknown <your email>
2022-03-22 15:06:52 -0400

    Pulling Components

---
+++ frontend/src/api/API.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+const KEY = 'AIzaSyBeOKBnq8xgVv2B5KhG_7v6icrln3Gzy3A';
+
+export default axios.create({
+    baseURL: 'youtube.googleapis.com',
+    params: 'snippet',
+    maxResults: 10,
+    key:KEY
+})
